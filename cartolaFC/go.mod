32b6ebdfc15c75c27be6ae9f10e8338d8f71d589
Felipe <felipeferreira0811@gmail.com>
2023-01-13 17:13:39 -0300

    (WIP) Cartola FC - Back and Dashboard

---
+++ cartolaFC/go.mod
@@ -0,0 +1,12 @@
+module github.com/fs-ferreira/projeto-cartola-fc
+
+go 1.19
+
+require github.com/google/uuid v1.3.0
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-sql-driver/mysql v1.7.0
+)
---
+++ cartolaFC/internal/usecase/my_team_add.go
@@ -0,0 +1,38 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/fs-ferreira/projeto-cartola-fc/internal/domain/entity"
+	"github.com/fs-ferreira/projeto-cartola-fc/internal/domain/repository"
+	"github.com/fs-ferreira/projeto-cartola-fc/pkg/uow"
+)
+
+type AddMyTeamInput struct {
+	ID    string
+	Name  string
+	Score int
+}
+
+type AddMyTeamUseCase struct {
+	Uow uow.UowInterface
+}
+
+func (a *AddMyTeamUseCase) Execute(ctx context.Context, input AddMyTeamInput) error {
+	myTeamRepository := a.getMyTeamRepository(ctx)
+	myTeam := entity.NewMyTeam(input.ID, input.Name)
+	err := myTeamRepository.Create(ctx, myTeam)
+	if err != nil {
+		return err
+	}
+	a.Uow.CommitOrRollback()
+	return nil
+}
+
+func (a *AddMyTeamUseCase) getMyTeamRepository(ctx context.Context) repository.MyTeamRepositoryInterface {
+	myTeamRepository, err := a.Uow.GetRepository(ctx, "MyTeamRepository")
+	if err != nil {
+		panic(err)
+	}
+	return myTeamRepository.(repository.MyTeamRepositoryInterface)
+}
